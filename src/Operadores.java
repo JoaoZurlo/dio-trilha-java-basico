c393a94a667e9363e2425ceee46798d994ab3a89
joao <jpzurlo.jz@gmail.com>
2023-02-28 22:26:08 -0300

    adicionar

---
+++ src/Operadores.java
@@ -0,0 +1,38 @@
+public class Operadores {
+    
+    public static void main(String[] args) {
+        //String nomeCompleto = "Linguagem " + "Java";
+        //System.out.println(nomeCompleto);
+
+        String concatenacao ="?";
+
+        concatenacao = 1+1+1+"1";
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+1;
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+1+1+1;
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+(1+1+1);
+        System.out.println(concatenacao);
+
+    }
+    // operadores --> comparação
+         // ==   ---->  igaul
+         // !=   ---->  diferente
+         // <    ---->  menor
+         // >    ---->  maior
+         // <=   ---->  menor igual
+         // =>   ---->  maior igual      
+    
+    // Operadores Aritmeticos --> comparação
+                 // +        ---->  SOMA
+                 // -        ---->  SUBTRAÇÃO
+                 // *        ---->  MULTIPLICAÇÃO
+                 // /        ---->  DIVISÃO
+                 // %        ---->  MODULO 
+    
+    
+}
